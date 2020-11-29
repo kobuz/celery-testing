d90476a8829585ca19175b6de266a1c1ffe61b7e
Marcin Sokół <kobbuz@gmail.com>
2020-11-29 22:41:06 +0100

    Remove redis vol2

--- project/celery_app.py
+++ project/celery_app.py
@@ -2,7 +2,7 @@ from celery import Celery
 
 app = Celery(
     "cabbage",
-    broker="redis://redis/0",
+    broker="redis://redis/0",  # both broker and backend are mentioned but not used
     backend="redis://redis/1",
 )
 
